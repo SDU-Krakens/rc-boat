f25066a56e0a7c96a8525c63f655ac9bdf53c3fc
vizn3r <viznertw@gmail.com>
2025-11-12 10:43:10 +0100

    Feat: GPIO basic operations

---
+++ lib/gpio/gpio.c
@@ -0,0 +1,112 @@
+#include "gpio.h"
+
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+void gpio_export(gpio_t *gpio) {
+  char *path;
+  asprintf(&path, "%s/gpiochip%d/export", GPIO_PATH, GPIO_OFFSET);
+
+  gpio->fd = open(path, O_WRONLY);
+  if (gpio->fd < 0) {
+    return;
+  }
+
+  char buf[8];
+  sprintf(buf, "%d", gpio->pin);
+  write(gpio->fd, buf, strlen(buf));
+  close(gpio->fd);
+  free(path);
+  path = NULL;
+
+  gpio->exported = true;
+}
+
+gpio_t *gpio_open(uint32_t pin) {
+  gpio_t *gpio = malloc(sizeof(gpio_t));
+  gpio->pin = pin;
+  gpio->direction = GPIO_INPUT;
+  gpio->value = 0;
+  gpio->exported = false;
+
+  gpio_export(gpio);
+
+  return gpio;
+}
+
+void gpio_close(gpio_t *gpio) {
+  free(gpio);
+  close(gpio->fd);
+}
+
+void gpio_set_direction(gpio_t *gpio, bool direction) {
+  gpio->direction = direction;
+
+  char *path;
+  asprintf(&path, "%s/gpio%d/direction", GPIO_PATH, gpio->pin + GPIO_OFFSET);
+
+  int fd = open(path, O_WRONLY);
+  if (fd < 0) {
+    return;
+  }
+
+  char buf[4];
+
+  if (direction == GPIO_INPUT)
+    sprintf(buf, "in");
+  else
+    sprintf(buf, "out");
+
+  write(fd, buf, strlen(buf));
+  close(fd);
+  free(path);
+  path = NULL;
+}
+
+void gpio_set_value(gpio_t *gpio, bool value) {
+  if (gpio->direction == GPIO_INPUT) {
+    return;
+  }
+
+  gpio->value = value;
+
+  char *path;
+  asprintf(&path, "%s/gpio%d/value", GPIO_PATH, gpio->pin + GPIO_OFFSET);
+
+  int fd = open(path, O_WRONLY);
+  if (fd < 0) {
+    return;
+  }
+
+  char buf[2];
+  sprintf(buf, "%d", value);
+  write(fd, buf, strlen(buf));
+  close(fd);
+  free(path);
+  path = NULL;
+}
+
+bool gpio_get_value(gpio_t *gpio) {
+  if (gpio->direction == GPIO_OUTPUT) {
+    return gpio->value;
+  }
+
+  char *path;
+  asprintf(&path, "%s/gpio%d/value", GPIO_PATH, gpio->pin + GPIO_OFFSET);
+
+  int fd = open(path, O_RDONLY);
+  if (fd < 0) {
+    return false;
+  }
+
+  char buf[64];
+  read(fd, buf, 2);
+  close(fd);
+  free(path);
+  path = NULL;
+
+  return true;
+}
