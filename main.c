e442d1df8b5e5693a7d3520939c0377fff773752
vizn3r <viznertw@gmail.com>
2025-11-17 17:54:28 +0100

    asdf

--- main.c
+++ main.c
@@ -73,9 +73,8 @@ int main(void) {
 
     if (message_len > 0 && message) {
       bool sent = lora_send(lora, message, message_len, 1000);
-      printf("accel: %f tilt: %d raw: %d %d %d gyro: %d %d %d sent: %d\n",
-             accel, tilt, accel_x, accel_y, accel_z, gyro_roll, gyro_pitch,
-             gyro_yaw, sent);
+      printf("Sent %d bytes: %s\n", (int)message_len, sent ? "OK" : "FAIL");
+      printf("Message: %s\n", message);
     }
 
     usleep(100000); // 100ms delay
