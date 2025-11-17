8aeacfe0035f924c550bb2345fc221d0c526f35d
vizn3r <viznertw@gmail.com>
2025-11-17 21:33:04 +0100

    fast push

--- main.c
+++ main.c
@@ -57,12 +57,12 @@ int main(void) {
   // Initialize IMU
   imuConfig();
 
-  lora_set_frequency(lora, 433E6);
-  lora_set_bandwith(lora, 125E3);
-  lora_set_spreading_factor(lora, 12);
-  lora_set_coding_rate(lora, 8);
-  lora_enable_crc(lora, true);
-  // lora_set_tx_power(lora, 17);
+  // lora_set_frequency(lora, 433E6);
+  // lora_set_bandwith(lora, 125E3);
+  // lora_set_spreading_factor(lora, 12);
+  // lora_set_coding_rate(lora, 8);
+  // lora_enable_crc(lora, true);
+  //  lora_set_tx_power(lora, 17);
   printf("LoRa initialized\n");
 
   while (1) {
