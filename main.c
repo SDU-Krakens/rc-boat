207b3240b8634193dfc1b2ecc4d29b87b55022c7
vizn3r <viznertw@gmail.com>
2025-11-17 17:32:57 +0100

    asdf

--- main.c
+++ main.c
@@ -38,7 +38,7 @@ int gyro_pitch = 0;
 int gyro_yaw = 0;
 
 int main(void) {
-  lora_t *lora = lora_init("/dev/spidev0.0", 1000000);
+  lora_t *lora = lora_init("/dev/spidev0.0", 1000000, 22);
   if (lora == NULL) {
     return 1;
   }
