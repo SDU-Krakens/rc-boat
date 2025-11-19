9beb3a8c010b21d2231431b7aaa4a4ddc977071d
vizn3r <viznertw@gmail.com>
2025-11-19 04:10:17 +0100

    test

--- test/test_testing.c
+++ test/test_testing.c
@@ -1,4 +1,5 @@
 #include "unity.h"
+#include <stdio.h>
 
 void setUp(void) {}
 
@@ -6,8 +7,11 @@ void tearDown(void) {}
 
 void test_pass(void) { TEST_ASSERT_TRUE(1); }
 
+void test_fail(void) { TEST_ASSERT_FALSE(0); }
+
 int main(void) {
   UNITY_BEGIN();
   RUN_TEST(test_pass);
+  RUN_TEST(test_fail);
   return UNITY_END();
 }
