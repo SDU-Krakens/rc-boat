72c1e7346fde1c58a90b6d3b48e335a2e13aaf79
vizn3r <viznertw@gmail.com>
2025-11-19 05:25:54 +0100

    im dying rn

--- test.sh
+++ test.sh
@@ -6,6 +6,9 @@ cd dist/test
 cmake -DCMAKE_BUILD_TYPE=Debug -DBUILD_TESTS=ON ../../
 cmake --build . -- -j$(nproc)
 
+echo "=== FILES IN BUILD DIR ==="
+find . -type f -executable
+
 cp ./compile_commands.json ../../compile_commands.json
 
 ctest -V --output-on-failure
