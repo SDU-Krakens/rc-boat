e94c6e8a1779333747392a8d09cfb4359c3a9dac
vizn3r <viznertw@gmail.com>
2025-11-19 05:03:58 +0100

    "fix"

--- test/test_testing.c
+++ test/test_testing.c
@@ -7,11 +7,8 @@ void tearDown(void) {}
 
 void test_pass(void) { TEST_ASSERT_TRUE(1); }
 
-void test_fail(void) { TEST_ASSERT_FALSE(0); }
-
 int main(void) {
   UNITY_BEGIN();
   RUN_TEST(test_pass);
-  RUN_TEST(test_fail);
   return UNITY_END();
 }
