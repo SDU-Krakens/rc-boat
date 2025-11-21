8582a0093472a6eaf23e0e8e6b3c4c5bfc3b1959
barbarian10204 <barbar10204ian@gmail.com>
2025-11-21 18:21:38 +0100

    sub slave controll added, but not tested

--- lib/imu/mpu6050.h
+++ lib/imu/mpu6050.h
@@ -12,8 +12,12 @@
 // #include <sys/types.h>
 #include "../i2c/i2c.h"
 #include <stdint.h>
+#include <sys/types.h>
 // defines vv
-#define adr_mpu 0x68 // specified by the breakout board we
+#define adr_mpu 0x68 // specified by the breakout board
+#define adr_slv0                                                               \
+	0xff // adress of the first sub slave (to be determined) // LSB 0 bu
+	     // default
 #define gyro_range                                                             \
 	0 // select which gyroscope range to use (see the table below) - default
 	  // is 0
@@ -33,14 +37,12 @@
 // see the mpu6000 register map for more information
 
 // function declatations  vv
-int i2c_read(char adr_slave,
-	     char adr_register); // returns the register value as a decimal
-int i2c_write(char adr_slave, char adr_register, char data);
 void get_raw_acc(int *xx, int *yy, int *zz);
 void get_raw_gyro(int *roll, int *pitch, int *yaw);
 void imu_config();
 void get_unfil_gyro(float *roll, float *pitch, float *yaw);
 void get_unfil_acc(float *x, float *y, float *z);
+void write_to_slave(u_int8_t adr_slave, u_int8_t adr_register, u_int8_t data);
 
 // global variables vv
 // /* defines the rang of values the gyro will suply.
@@ -65,3 +67,8 @@ u_int8_t dlpf = 0; // 3bit // changes depending on the bandwidth and delay of
 		   // gyro and accelerometer (see register map page 13)
 u_int8_t ext_synq = 0;	   // 3bit // no syncing
 u_int8_t sample_rate = 10; // [khz]
+u_int8_t wait_for_es =
+    1; // 1bit // if 1: data ready interrupt waits for the slave data to arrive
+u_int8_t i2c_mst_clk = 0; // 4bit // determins the clock frequency of the i2c
+			  // master (see register map page 18)
+u_int8_t
