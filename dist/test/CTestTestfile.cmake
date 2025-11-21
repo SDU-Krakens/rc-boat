c8908b12330a353a795fcd5a6a4dfe0981adf6fb
vizn3r <viznertw@gmail.com>
2025-11-21 14:46:10 +0100

    fast push

--- dist/test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/vizn3r/dev/kraken/rc-boat
-# Build directory: /home/vizn3r/dev/kraken/rc-boat/dist/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test_testing "/home/vizn3r/dev/kraken/rc-boat/dist/test/test_testing")
-set_tests_properties(test_testing PROPERTIES  _BACKTRACE_TRIPLES "/home/vizn3r/dev/kraken/rc-boat/CMakeLists.txt;79;add_test;/home/vizn3r/dev/kraken/rc-boat/CMakeLists.txt;0;")
