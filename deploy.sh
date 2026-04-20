44e77c8955aa41b8d8c599f1c10bb3021742a2fd
vizn3r <viznertw@gmail.com>
2026-04-20 18:18:29 +0200

    fast push

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,6 @@ set -e
 cd "$(dirname "$0")"
 git fetch --all
 git reset --hard origin/main
-cmake -B build
-cmake --build build
+./build.sh
 systemctl restart boat
 echo "Done. Binary: build/rc-boat"
